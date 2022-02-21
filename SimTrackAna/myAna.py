21886bc468482e0e0f3559546ecece7a562ed6b5
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2022-02-21 09:53:32 +0100

    Update summary plots

--- SimTrackAna/myAna.py
+++ SimTrackAna/myAna.py
@@ -27,9 +27,14 @@ c1.SetTicks(1,1)
 c1.SetLeftMargin(0.12)
 c1.SetRightMargin(0.08)
 
+c2 = ROOT.TCanvas("c2", "", 5600, 2400)
+c2.SetLeftMargin(0.)
+c2.SetRightMargin(0.)
+c2.Divide(7,4)
+
 #--------------------------------------------------
 
-def make_plot(varName, bool_make_profile):
+def make_plot(myRootfiles, varName, bool_make_logitudinal_profile):
 
     dir_output = eos + "/" + pu.sub_directory[varName]
     if not os.path.isdir(dir_output):
@@ -39,58 +44,106 @@ def make_plot(varName, bool_make_profile):
     processes = ["1", "13", "26"]
     processes = [str(i) for i in range(1,27)]
 
-    v_hists = []
-    if "Eta" in varName or "Phi" in varName:
-        v_hists = pu.load_single_histogram(fin, varName)
-    else:
-        v_hists = pu.load_histograms(fin, varName, processes)
+    # loading
+    vf = []
+    v_v_hists = []
+    for rootfile in myRootfiles:
+        fin = ROOT.TFile.Open(rootfile, "R")
+        vf.append(fin)
+
+        v_hists = []
+        if "Eta" in varName or "Phi" in varName:
+            v_hists = pu.load_single_histogram(fin, varName)
+            v_v_hists.append(v_hists)
+        else:
+            v_hists = pu.load_histograms(fin, varName, processes)
+            v_v_hists.append(v_hists)
 
     # longitdinal profile
-    if bool_make_profile:
-        gr = pu.get_graph(varName, v_hists)
-        gr.Draw("apc")
-        output = eos + "/" + varName
-        c1.SaveAs(output + ".png")
+    if bool_make_logitudinal_profile:
+        v_gr = []
 
-    # individual plots
-    for i, h in enumerate(v_hists):
-        h.Draw()
-        output = dir_output + "/" + varName + "_" + processes[i]
+        # loop over energy
+        for i, v_hists in enumerate(v_v_hists):
+            gr = pu.get_graph(varName, v_hists)
+            gr.SetLineColor(colors[i])
+            gr.SetMarkerColor(colors[i])
+            v_gr.append(gr)
+
+            # 26 plots in one
+            c2.cd()
+            for j, h in enumerate(v_hists):
+                c2.cd(j+1)
+                c2.SetGrid()
+                c2.SetTicks(1,1)
+                h.Draw()
+
+            output = eos + "/" + varName + "_all_plots_" + tags[i]
+            c2.SaveAs(output + ".png")
+            c2.SaveAs(output + ".pdf")
+
+            # individual plots
+            c1.cd()
+            for j, h in enumerate(v_hists):
+                h.Draw()
+                output = dir_output + "/" + varName + "_" + processes[j] + "_" + tags[i]
+                c1.SaveAs(output + ".png")
+                c1.SaveAs(output + ".pdf")
+
+        # logitudinal profile
+        c1.cd()
+        for i, gr in enumerate(v_gr):
+            if i==0: gr.Draw("apc")
+            else:    gr.Draw('pc;same')
+
+        output = eos + "/" + varName
         c1.SaveAs(output + ".png")
         c1.SaveAs(output + ".pdf")
 
 #--------------------------------------------------
 
-def run():
-    c1.cd()
-
-    make_plot("hEta", False )
-    make_plot("hPhi", False )
+def run(input_files):
+    make_plot(input_files, "hEta", False )
+    make_plot(input_files, "hPhi", False )
 
     thickness = ["120mum", "200mum", "300mum", "total"]
     thickness = ["total"] # consider 120, 200, 300 altogether
 
     for t in thickness:
 
-        make_plot("ADC_SimhitE_%s_layer"    % t , False )
-        make_plot("ADC_MIP_%s_layer"        % t , False )
-        make_plot("MIP_SimhitE_%s_layer"    % t , False )
+        make_plot(input_files, "total_MIP_%s"            % t , True  )
 
         continue
 
-        make_plot("total_ADC_%s"            % t , True  )
-        make_plot("total_MIP_%s"            % t , True  )
-        make_plot("total_SIM_%s"            % t , True  )
-        make_plot("multiplicity_digis_%s"   % t , True  )
-        make_plot("multiplicity_simhits_%s" % t , True  )
+        make_plot(input_files, "total_ADC_%s"            % t , True  )
+        make_plot(input_files, "total_MIP_%s"            % t , True  )
+        make_plot(input_files, "total_SIM_%s"            % t , True  )
+        make_plot(input_files, "multiplicity_digis_%s"   % t , True  )
+        make_plot(input_files, "multiplicity_simhits_%s" % t , True  )
+
+        make_plot(input_files, "ADC_SimhitE_%s_layer"    % t , False )
+        make_plot(input_files, "ADC_MIP_%s_layer"        % t , False )
+        make_plot(input_files, "MIP_SimhitE_%s_layer"    % t , False )
 
         continue
 
-        make_plot("ADC_%s"            % t , True  )
-        make_plot("MIP_%s"            % t , True  )
-        make_plot("SIM_%s"            % t , True  )
+        make_plot(input_files, "ADC_%s"            % t , True  )
+        make_plot(input_files, "MIP_%s"            % t , True  )
+        make_plot(input_files, "SIM_%s"            % t , True  )
 
 if __name__ == "__main__":
-    run()
+    colors = [ROOT.kBlack, ROOT.kBlue, ROOT.kRed]
+    myRootfiles = [
+        "rootfiles/geantoutput_D86_R35To60_E300.root",
+        "rootfiles/geantoutput_D86_R35To60_E100.root",
+        "rootfiles/geantoutput_D86_R35To60_E20.root",
+        "rootfiles/geantoutput_D86_R80To100_E300.root",
+        "rootfiles/geantoutput_D86_R80To100_E100.root",
+        "rootfiles/geantoutput_D86_R80To100_E20.root",
+    ]
+
+    tags = ["E300", "E100", "E20"]
+
+    run( myRootfiles[0:3] )
     #subprocess.call("ls -lhrt %s" % dir_output, shell=True)
 
