dcd5d67a43def994287ba92398f5d4ec2174bb86
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2022-02-23 11:25:45 +0100

    Include scripts for plotting test beam data

---
+++ testbeam/myAna.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+import os, subprocess
+import ROOT
+ROOT.gROOT.SetBatch(True)
+#ROOT.gStyle.SetOptStat("emr")
+ROOT.gStyle.SetOptStat(2210)
+ROOT.gStyle.SetOptFit(1111)
+
+my_stat_pos = [0.89, 0.87, 0.24, 0.08]
+ROOT.gStyle.SetStatX(my_stat_pos[0])
+ROOT.gStyle.SetStatY(my_stat_pos[1])
+ROOT.gStyle.SetStatW(my_stat_pos[2])
+ROOT.gStyle.SetStatH(my_stat_pos[3])
+ROOT.gStyle.SetTextSize(1.2)
+
+import my_plot_utils as pu
+
+eos = "./eos_output"
+rootfile = eos + "/" + "geantoutput_v3p1.root"
+rootfile = "geantoutput_v3p1.root"
+rootfile = "rootfiles/geantoutput_D86_R80To100_E100.root"
+#fin = ROOT.TFile.Open(rootfile, "R")
+
+c1 = ROOT.TCanvas("c1", "", 800, 600)
+c1.SetGrid()
+c1.SetTicks(1,1)
+c1.SetLeftMargin(0.12)
+c1.SetRightMargin(0.08)
+
+c2 = ROOT.TCanvas("c2", "", 5600, 2400)
+c2.SetLeftMargin(0.)
+c2.SetRightMargin(0.)
+c2.Divide(7,4)
+
+#--------------------------------------------------
+def create_directory(dir_output):
+    if not os.path.isdir(dir_output):
+        subprocess.call("mkdir %s" % dir_output, shell=True)
+        subprocess.call("cp -p %s/index.php %s" % (eos, dir_output), shell=True)
+
+def annotate(rshift=0):
+    latex = ROOT.TLatex()
+    latex.SetNDC()
+    latex.SetTextFont(43)
+    latex.SetTextAlign(11)
+    #latex.SetTextSize(24)
+    latex.SetTextSize(20)
+    latex.DrawLatex( 0.12, 0.912, "#bf{CMS} #it{work in progress}" )
+    latex.DrawLatex( 0.58+rshift, 0.912, "D86 Simulation with 1,000 events" )
+    #latex.DrawLatex( 0.69+rshift, 0.912, "%s fb^{-1} (13 TeV)" % str(lumi["RunII"]) )
+
+def make_plot(varName, bool_make_logitudinal_profile):
+    global myRootfiles, specified_directory
+    
+    # Initiate
+    bool_this_is_eta_phi = "Eta" in varName or "Phi" in varName
+    #dir_output = specified_directory + "/" + pu.sub_directory[varName]
+    dir_output = specified_directory
+    create_directory(dir_output)
+    processes = [str(i) for i in range(1,27)]
+    processes = [str(i) for i in range(1,29)]
+
+    # Load histograms
+    vf = []
+    v_v_hists = []
+    for rootfile in myRootfiles:
+        fin = ROOT.TFile.Open(rootfile, "R")
+        vf.append(fin)
+
+        v_hists = []
+        if bool_this_is_eta_phi:
+            v_hists = pu.load_single_histogram(fin, varName)
+            v_v_hists.append(v_hists)
+        else:
+            v_hists = pu.load_histograms(fin, varName, processes)
+            v_v_hists.append(v_hists)
+
+    # Eta or Phi
+    if bool_this_is_eta_phi:
+        c1.cd()
+        for i, v_hists in enumerate(v_v_hists):
+            v_hists[0].Draw()
+            output = dir_output + "/" + varName + "_" + tags[i]
+            c1.SaveAs(output + ".png")
+            c1.SaveAs(output + ".pdf")
+
+    # Longitdinal profile
+    if bool_make_logitudinal_profile:
+        v_gr = []
+
+        # loop over energy
+        for i, v_hists in enumerate(v_v_hists):
+            gr = pu.get_graph(varName, v_hists)
+            gr.SetLineColor(colors[i])
+            gr.SetMarkerColor(colors[i])
+            v_gr.append(gr)
+
+            # 26 plots in one
+            c2.cd()
+            for j, h in enumerate(v_hists):
+                c2.cd(j+1)
+                c2.SetGrid()
+                c2.SetTicks(1,1)
+                h.Draw()
+
+            output = specified_directory + "/" + varName + "_all_plots_" + tags[i]
+            c2.SaveAs(output + ".png")
+            c2.SaveAs(output + ".pdf")
+
+            continue
+
+            # individual plots
+            c1.cd()
+            for j, h in enumerate(v_hists):
+                h.Draw()
+                output = dir_output + "/" + varName + "_" + processes[j] + "_" + tags[i]
+                c1.SaveAs(output + ".png")
+                c1.SaveAs(output + ".pdf")
+
+        # logitudinal profile
+        c1.cd()
+        legend = ROOT.TLegend(0.67, 0.65, 0.87, 0.85)
+        legend.SetLineColor(0)
+        legend.SetTextSize(0.04)
+        for i, gr in enumerate(v_gr):
+            if i==0: gr.Draw("apc")
+            else:    gr.Draw('pc;same')
+            legend.AddEntry(gr, label[tags[i]], "l")
+
+        annotate()
+        legend.Draw("same")
+
+        output = specified_directory + "/" + varName
+        c1.SaveAs(output + ".png")
+        c1.SaveAs(output + ".pdf")
+
+#--------------------------------------------------
+
+def run(myfin, mydin):
+    global myRootfiles, specified_directory
+    myRootfiles = myfin
+    specified_directory = mydin
+
+    create_directory( specified_directory )
+
+    thickness = ["total"] # consider 120, 200, 300 altogether
+
+    for t in thickness:
+        make_plot( "rechit_amplitudeHigh_%s" % t , True  )
+        make_plot( "rechit_amplitudeLow_%s"  % t , True  )
+
+#--------------------------------------------------
+
+if __name__ == "__main__":
+    tags = ["E300", "E100", "E20"]
+    label = {}
+    label["E300"] = "300 GeV"
+    label["E100"] = "100 GeV"
+    label["E20"]  = "20 GeV"
+
+    colors = [ROOT.kBlack, ROOT.kBlue, ROOT.kRed]
+    input_files = [
+        "output/my_test_beam_data_e300.root",
+        "output/my_test_beam_data_e100.root",
+        "output/my_test_beam_data_e20.root",
+    ]
+
+    myRootfiles, specified_directory = [], ""
+    run( input_files[0:3], eos + "/" + "data"  )
+
---
+++ testbeam/my_plot_utils.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+import math
+import copy
+import array
+import ROOT
+ROOT.gROOT.SetBatch(True)
+
+def load_single_histogram(fin, varName):
+    v_hists = []
+    histName = varName
+    h = fin.Get(histName)
+    v_hists.append(h)
+
+    return v_hists
+
+def load_histograms(fin, varName, processes):
+    v_hists = []
+    for p in processes:
+        histName = varName + "_" + str(p)
+        h = fin.Get(histName)
+        v_hists.append(h)
+
+    return v_hists
+
+def set_graph(gr, ytitle, xtitle, color):
+    gr.SetTitle("")
+    gr.SetLineColor(color)
+    gr.SetLineWidth(2)
+    gr.SetMarkerStyle(20)
+    gr.SetMarkerColor(color)
+    gr.SetMinimum(0)
+    gr.GetXaxis().SetTitle(xtitle)
+    gr.GetYaxis().SetTitle(ytitle)
+    gr.GetXaxis().SetTitleOffset(1.2)
+
+sub_directory = {
+    "hEta"                        : "EtaPhi",
+    "hPhi"                        : "EtaPhi",
+
+    "ADC_total_layer"             : "ADC",
+    "ADC_120mum_layer"            : "ADC",
+    "ADC_200mum_layer"            : "ADC",
+    "ADC_300mum_layer"            : "ADC",
+    "MIP_total_layer"             : "MIP",
+    "MIP_120mum_layer"            : "MIP",
+    "MIP_200mum_layer"            : "MIP",
+    "MIP_300mum_layer"            : "MIP",
+    "SIM_total_layer"             : "SIM",
+    "SIM_120mum_layer"            : "SIM",
+    "SIM_200mum_layer"            : "SIM",
+    "SIM_300mum_layer"            : "SIM",
+    "ADC_SimhitE_total_layer"     : "ADC_SimhitE",
+    "ADC_SimhitE_120mum_layer"    : "ADC_SimhitE",
+    "ADC_SimhitE_200mum_layer"    : "ADC_SimhitE",
+    "ADC_SimhitE_300mum_layer"    : "ADC_SimhitE",
+    "ADC_MIP_total_layer"         : "ADC_MIP",
+    "ADC_MIP_120mum_layer"        : "ADC_MIP",
+    "ADC_MIP_200mum_layer"        : "ADC_MIP",
+    "ADC_MIP_300mum_layer"        : "ADC_MIP",
+    "MIP_SimhitE_total_layer"     : "MIP_SimhitE",
+    "MIP_SimhitE_120mum_layer"    : "MIP_SimhitE",
+    "MIP_SimhitE_200mum_layer"    : "MIP_SimhitE",
+    "MIP_SimhitE_300mum_layer"    : "MIP_SimhitE",
+
+    "total_ADC_total"             : "total_ADC",
+    "total_ADC_120mum"            : "total_ADC",
+    "total_ADC_200mum"            : "total_ADC",
+    "total_ADC_300mum"            : "total_ADC",
+    "total_MIP_total"             : "total_MIP",
+    "total_MIP_120mum"            : "total_MIP",
+    "total_MIP_200mum"            : "total_MIP",
+    "total_MIP_300mum"            : "total_MIP",
+    "total_SIM_total"             : "total_SIM",
+    "total_SIM_120mum"            : "total_SIM",
+    "total_SIM_200mum"            : "total_SIM",
+    "total_SIM_300mum"            : "total_SIM",
+    "multiplicity_digis_total"    : "multiplicity_digis",
+    "multiplicity_digis_120mum"   : "multiplicity_digis",
+    "multiplicity_digis_200mum"   : "multiplicity_digis",
+    "multiplicity_digis_300mum"   : "multiplicity_digis",
+    "multiplicity_simhits_300mum" : "multiplicity_simhits",
+    "multiplicity_simhits_total"  : "multiplicity_simhits",
+    "multiplicity_simhits_120mum" : "multiplicity_simhits",
+    "multiplicity_simhits_200mum" : "multiplicity_simhits",
+
+    "rechit_amplitudeHigh_total" : "amplitudeHigh",
+    "rechit_amplitudeLow_total"  : "amplitudeLow",
+}
+
+ytitles = copy.deepcopy( sub_directory )
+ytitles["hEta"] = "Eta"
+ytitles["hPhi"] = "Phi"
+
+x_D86 = [0.564,1.567,2.547,3.549,4.528,5.531,6.509,7.512,8.49,9.493,10.472,11.474,12.453,13.455,14.434,15.437,16.415,17.418,18.975,19.978,21.536,22.538,24.096,25.099,26.656,27.659]
+
+def get_graph(varName, v_hists):
+    #n = 26 
+    n = 28 
+    x, ex = array.array('d'), array.array('d')
+    y, ey = array.array('d'), array.array('d')
+
+    for i, h in enumerate(v_hists):
+        #x.append(x_D86[i])
+        x.append(float(i+1))
+        y.append(h.GetMean())
+        ex.append(0.)
+        #ey.append(h.GetStdDev())
+        ey.append(h.GetMeanError())
+
+        gr = ROOT.TGraphErrors(n, x, y, ex, ey)
+        #set_graph(gr, ytitles[varName], "Layer depth [ X_{0} ]", ROOT.kBlue)
+        set_graph(gr, ytitles[varName], "Layer", ROOT.kBlue)
+
+    return gr
